bb49b0869b95c96c1e0394c85607770e2e6d67f8
Alex Buaiscia <alex.buaiscia@24i.com>
2020-07-28 16:18:44 +0200

    fix: lint on sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -18,8 +18,8 @@ const Sidebar = ({ isIndex }: Props) => {
     <div className={styles['sidebar']}>
       <div className={styles['sidebar__inner']}>
         <Author author={author} isIndex={isIndex} />
-        <p style={{fontWeight: 800}}>Would you like to know more?</p>
-        <a style={{fontWeight: 800}} href="http://www.alexbuaiscia.com/" target="__blank">I'm doing my part, on alexbuaiscia.com</a>
+        <p style={{ fontWeight: 800 }}>Would you like to know more?</p>
+        <a style={{ fontWeight: 800 }} href="http://www.alexbuaiscia.com/" target="__blank">I'm doing my part, on alexbuaiscia.com</a>
         <br></br>
         <br></br>
         <Menu menu={menu} />
