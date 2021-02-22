2efdbd8f30691efc12cf2638820efdefc8f30a26
Alex Buaiscia <alex.buaiscia@gmail.com>
2021-02-22 15:00:05 +0100

    add exportpathmap

--- next.config.js
+++ next.config.js
@@ -10,15 +10,12 @@ module.exports = withBundleAnalyzer({
   experimental: {
     modern: true,
   },
-  exportPathMap: async (defaultPathMap, { dev, dir, outDir, distDir, buildId }) => {
+  exportPathMap: function () {
     return {
       '/': { page: '/' },
       '/blog': { page: '/blog' },
       '/tags': { page: '/tags' },
       '/about': { page: '/about' },
-      // '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
-      // '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
-      // '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
     }
   },
   webpack: (config, { dev, isServer }) => {
