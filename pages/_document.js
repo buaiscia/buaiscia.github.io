6d9d39a0f3749f7ba5d643a5ba255784a5007aaa
Alex Buaiscia <alex.buaiscia@gmail.com>
2022-01-25 21:43:39 +0100

    add ga

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,20 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          ></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`,
+            }}
+          />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
