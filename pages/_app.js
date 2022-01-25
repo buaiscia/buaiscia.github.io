4010bd37ffec48a916bb06988cc4ae8fa1739781
Alex Buaiscia <alex.buaiscia@gmail.com>
2022-01-25 21:11:10 +0100

    add google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '@/css/tailwind.css'
-
+import Script from 'next/script'
 import { MDXProvider } from '@mdx-js/react'
 import { ThemeProvider } from 'next-themes'
 import { DefaultSeo } from 'next-seo'
@@ -15,6 +15,21 @@ export default function App({ Component, pageProps }) {
       <MDXProvider components={MDXComponents}>
         <Head>
           <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+
+          <Script id="google-analytics" strategy="lazyOnload">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+          </Script>
         </Head>
         <DefaultSeo {...SEO} />
         <LayoutWrapper>
